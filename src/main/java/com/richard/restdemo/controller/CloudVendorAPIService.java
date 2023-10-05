a9a4823b2a9ef268b2f7a273801ee3bb05a13fb2
Richard Nnaji <richardnnaji@macbooks-MacBook-Pro.local>
2023-10-05 11:30:54 +0100

    feat: My first feature

---
+++ src/main/java/com/richard/restdemo/controller/CloudVendorAPIService.java
@@ -0,0 +1,32 @@
+package com.richard.restdemo.controller;
+
+import com.richard.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId){
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor update Successfully";
+    }
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
