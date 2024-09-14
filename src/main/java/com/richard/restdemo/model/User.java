8ad21117f20b9ea05980a4d05cbb4868fb65419f
emezieoluchi29 <emezieoluchi29@gmail.com>
2024-09-14 11:35:32 +0100

    update changes

---
+++ src/main/java/com/richard/restdemo/model/User.java
@@ -0,0 +1,52 @@
+package com.richard.restdemo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public User(Long id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
